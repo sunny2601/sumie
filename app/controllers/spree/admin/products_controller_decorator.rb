56d39da50c2e576105289087d10a685a6bf229c1
Carlos Ramos <carakan@gmail.com>
2014-10-18 19:42:51 -0400

    Fix simple error for check role admin/vendor

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -3,12 +3,14 @@ Spree::Admin::ProductsController.class_eval do
   before_filter :before_index, only: [:index]
 
   def before_index
-    @collection = @collection.where('supplier_id = ?', spree_current_user.supplier.id) if spree_current_user.has_spree_role?('vendor')
+    if spree_current_user.supplier
+      @collection = @collection.where('supplier_id = ?', spree_current_user.supplier.id) if spree_current_user.has_spree_role?('vendor')
+    end
   end
 
   def before_create
-    return false if spree_current_user.has_spree_role?('admin')
-
-    params[:product][:supplier_id] = spree_current_user.supplier.id
+    if spree_current_user.supplier
+      params[:product][:supplier_id] = spree_current_user.supplier.id
+    end
   end
 end
