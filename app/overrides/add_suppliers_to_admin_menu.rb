6a24dd2c13ca88080ec79e93c7558142bbb1f66a
Carlos Ramos <carakan@gmail.com>
2014-10-24 17:25:43 -0400

    changes in url

--- app/overrides/add_suppliers_to_admin_menu.rb
+++ app/overrides/add_suppliers_to_admin_menu.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: "spree/admin/shared/_menu",
                      name: "suppliers_admin_tab",
                      insert_bottom: "[data-hook='admin_tabs']",
-                     text: "<%= tab(Spree.t('suppliers'), :label => Spree.t('suppliers_label')) %>")
+                     text: "<%= tab(:suppliers, :label => Spree.t('suppliers')) %>")
